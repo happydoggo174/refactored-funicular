1b948fba901fa8c7d1b8f47d6dea9a6303141492
happydoggo174 <nguyenhaithanh26022009@gmail.com>
2026-02-27 22:08:39 +0700

    add reaction capabilities

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ let auth_header=sessionStorage.getItem('auth');
 if(auth_header!=null){
     auth_header={"Authorization":auth_header};
 }
+export function is_authenticated(){return auth_header!=null;}
 export async function check_login(username,password){
     const form_data=new FormData();
     form_data.append("username",username);
@@ -78,8 +79,12 @@ export async function get_post(){
 }
 export async function get_post_detail(post_id) {
     const url=`${VERCEL_URL}/post/detail/${post_id}`;
+    let headers={};
+    if(auth_header!=null){
+        headers=auth_header;
+    }
     try{
-        const resp=await fetch(url);
+        const resp=await fetch(url,{headers:headers});
         if(!resp.ok){return null;}
         return await resp.json();
     }catch{
@@ -90,11 +95,12 @@ function get_post_id(){
     return (new URL(window.location.href)).searchParams.get('post_id');
 }
 export async function like_post(){
+    if(auth_header==null){return false;}
     const post_id=get_post_id();
     const url=new URL(VERCEL_URL.concat("/post/like"));
     url.searchParams.append('post_id',post_id);
     try{
-        const resp=await fetch(url,{method:"POST",});
+        const resp=await fetch(url,{method:"POST",headers:auth_header});
         if(!resp.ok){return false;}
     }catch{
         return false;
@@ -102,11 +108,12 @@ export async function like_post(){
     return true;
 }
 export async function dislike_post(){
+    if(auth_header==null){return false;}
     const post_id=get_post_id();
     const url=new URL(VERCEL_URL.concat("/post/dislike"));
     url.searchParams.append('post_id',post_id);
     try{
-        const resp=await fetch(url,{method:"POST",});
+        const resp=await fetch(url,{method:"POST",headers:auth_header});
         if(!resp.ok){return false;}
     }catch{
         return false;
